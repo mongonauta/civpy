4848fe1e33a406fe17ba1d6c35f1473da589bdaf
CaptainHennessey <pingislands@gmail.com>
2014-01-05 14:35:47 +0100

    Revert "Committing for first time"
    This reverts commit 638c8b0e4bd9ed8653e9bad53be0582c685c51bb.

--- game/rainfall.py
+++
@@ -1,9 +0,0 @@
-import math
-import random
-
-
-
-def generateRainfallMap(altitudeMap):
-  vsize = len(altitudeMap)
-  hsize = len(altitudeMap[0])
-  return [ [random.random()]*hsize for i in range(vsize) ]
