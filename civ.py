bd55bf9a5fbb223c4ca3d3b5d773b445557a67bc
CaptainHennessey <pingislands@gmail.com>
2014-02-13 23:25:39 +0100

    Added HUD
    HUD shows attributes of a tile when hovered over. Some duplicate code
    concerning mouse events still exists, decide what to do with this later.

--- civ.py
+++ civ.py
@@ -13,15 +13,83 @@ class HeadUpDisplay(cocos.layer.Layer):
   def __init__(self):
     super( HeadUpDisplay, self ).__init__()
     
-    label = cocos.text.Label('HUD text',
-                            font_name='Times New Roman',
-                            font_size=16,
-                            anchor_x='center', anchor_y='center')
-                
-    label.position = 100,500
-    self.add(label)
+    self.label1 = cocos.text.Label('HUD text 1', anchor_x='left', anchor_y='bottom')
+    self.label2 = cocos.text.Label('HUD text 2', anchor_x='left', anchor_y='bottom')
+    self.label3 = cocos.text.Label('HUD text 3', anchor_x='left', anchor_y='bottom')
+    self.label4 = cocos.text.Label('HUD text 4', anchor_x='left', anchor_y='bottom')
+    self.label5 = cocos.text.Label('HUD text 5', anchor_x='left', anchor_y='bottom')
+    self.label6 = cocos.text.Label('HUD text 5', anchor_x='left', anchor_y='bottom')
+                 
+    self.label1.position = 0,0
+    self.label2.position = 0,16
+    self.label3.position = 0,32
+    self.label4.position = 0,48
+    self.label5.position = 0,64
+    self.label6.position = 0,80
+        
+    self.add(self.label1)
+    self.add(self.label2)
+    self.add(self.label3)
+    self.add(self.label4)
+    self.add(self.label5)
+    self.add(self.label6)
              
+  #def changeHud(self, label1text, label2text, label3text):
+  #  self.label1.element.text = label1text
+  #  self.label2.element.text = label2text
+  #  self.label3.element.text = label3text
              
+     
+class MouseDisplay(cocos.layer.Layer):
+
+    is_event_handler = True     #: enable director.window events
+
+    def __init__(self):
+        super( MouseDisplay, self ).__init__()
+
+        self.posx = 0
+        self.posy = 0
+        self.text = cocos.text.Label('No mouse events yet', font_size=18, x=self.posx, y=self.posy, anchor_x='left', anchor_y='bottom' )
+        self.add( self.text )
+
+    def update_text (self, x, y):
+        text = 'Mouse @ %d,%d' % (x, y)
+        self.text.element.text = text
+        self.text.element.x = self.posx
+        self.text.element.y = self.posy
+
+    def on_mouse_motion (self, x, y, dx, dy):
+        """Called when the mouse moves over the app window with no button pressed
+
+        (x, y) are the physical coordinates of the mouse
+        (dx, dy) is the distance vector covered by the mouse pointer since the
+          last call.
+        """
+        self.update_text (x, y)
+
+    def on_mouse_drag (self, x, y, dx, dy, buttons, modifiers):
+        """Called when the mouse moves over the app window with some button(s) pressed
+
+        (x, y) are the physical coordinates of the mouse
+        (dx, dy) is the distance vector covered by the mouse pointer since the
+          last call.
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+           (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.update_text (x, y)
+
+    def on_mouse_press (self, x, y, buttons, modifiers):
+        """This function is called when any mouse button is pressed
+
+        (x, y) are the physical coordinates of the mouse
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+           (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.posx, self.posy = director.get_virtual_coordinates (x, y)
+        self.update_text (x,y)        
+        
                 
 # load world from file
 def loadWorld():
@@ -39,7 +107,7 @@ def main():
   global keyboard, mouse, scroller, world, graphics
   
   from cocos.director import director
-  director.init(width=800, height=600, do_not_scale=True, resizable=True)
+  director.init(width=512, height=512, do_not_scale=True, resizable=True)
 
   # load world
   world = loadWorld()
@@ -58,7 +126,35 @@ def main():
   director.window.push_handlers(keyboard)
     
   def on_mouse_motion(x, y, dx, dy):
-    graphics.scroller.set_focus(x * 1024 / 800, y * 1024 / 600)
+  
+    # set focus
+    graphics.scroller.set_focus(x, y)
+    
+    # determine pixel coordinates of cursor
+    mx, my = graphics.scroller.pixel_from_screen(x, y)
+    
+    # determine which cell cursor is at
+    layer = graphics.scroller.get('land')
+    cell = layer.get_at_pixel(mx, my)
+    
+    # change HUD to display attributes of cell
+    if cell:
+     j = cell.position[1]/16
+     i = cell.position[0]/16
+     altitude = world.resourceMap['altitude'][i][j]
+     forest = world.resourceMap['forest'][i][j]
+     human = world.resourceMap['human'][i][j]
+       
+     hud_layer.label1.element.text = 'altitude: ' + str(world.resourceMap['altitude'][i][j])
+     hud_layer.label2.element.text = 'forest: ' + str(world.resourceMap['forest'][i][j])
+     hud_layer.label3.element.text = 'human: ' + str(world.resourceMap['human'][i][j])
+     hud_layer.label4.element.text = 'land: ' + str(world.resourceMap['land'][i][j])
+     hud_layer.label5.element.text = 'temperature: ' + str(world.resourceMap['temperature'][i][j])
+     hud_layer.label6.element.text = 'coordinates (x,y) / (j,i): (' + str(mx) + ',' + str(my) + ') / (' + str(j) + ',' + str(i) + ')'
+     
+     
+     #hud_layer.changeHud(altitude, forest, human)
+     
       
   def on_key_press(key, modifier):
     if key == pyglet.window.key.ENTER:
@@ -69,9 +165,9 @@ def main():
       
     elif key == pyglet.window.key.Z:
       if graphics.scroller.scale == .50:
-        graphics.scroller.do(actions.ScaleTo(1, 1))
+        graphics.scroller.do(actions.ScaleTo(1, 0))
       else:
-        graphics.scroller.do(actions.ScaleTo(.50, 1))
+        graphics.scroller.do(actions.ScaleTo(.50, 0))
           
   director.window.push_handlers(on_key_press)
   director.window.push_handlers(on_mouse_motion)    
